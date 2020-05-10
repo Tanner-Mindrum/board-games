765d6f467e8300ced024608410cc3010b09fec28
Tanner-Mindrum <track2gamesofficial@gmail.com>
2020-05-09 22:14:01 -0700

    updated with fasicle 5 stuff

--- src/Cecs475.BoardGames.WpfApp/GameChoiceWindow.xaml.cs
+++ src/Cecs475.BoardGames.WpfApp/GameChoiceWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Cecs475.BoardGames.WpfView;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -30,8 +31,14 @@ namespace Cecs475.BoardGames.WpfApp {
 						};
 
 			foreach (var dllFile in dlls) {
-				if (System.IO.Path.GetExtension(dllFile.File) == ".dll")
+				if (System.IO.Path.GetExtension(dllFile.File) == ".dll") {
+					/*Console.WriteLine(dllFile.File);
+					string filename = System.IO.Path.GetFileName(dllFile.File.Substring(0, dllFile.File.Length - 4));
+					Console.WriteLine(filename);
+					string version = FileVersionInfo.GetVersionInfo(dllFile.File).FileVersion;
+					Assembly.Load(filename + ", Version=" + version + ", Culture=neutral, " + "PublicKeyToken=68e71c13048d452a");*/
 					Assembly.LoadFrom(dllFile.File);
+				}
 			}
 
 			var finalTypes = new List<Type>();
